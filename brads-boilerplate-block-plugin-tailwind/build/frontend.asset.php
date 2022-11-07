d2d6ffa84d69162a02439f5ca3f25d6210e26e24
tevfiksirin <tevfiksirin@gmail.com>
2022-11-07 18:25:13 +0300

    temizlik

--- brads-boilerplate-block-plugin-tailwind/build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'af46fc80bb0f8dce8a59');
--- brads-boilerplate-block-plugin-tailwind/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '87bdc8cacba082715c32');
